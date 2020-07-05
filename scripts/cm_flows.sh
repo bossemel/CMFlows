6e07e6dba1e5dd9ba780dbbb8ea320433e07725a
bossemel <18574762+bossemel@users.noreply.github.com>
2020-07-05 13:33:43 +0100

    change script

--- scripts/cm_flows.sh
+++ scripts/cm_flows.sh
@@ -12,18 +12,18 @@ set -e
 # --obs 10000 \
 # --pretrain_models
 
-# echo 'Begin Copula: Clayton, Margina: bimodal_gaussian, CM Flow only'
+echo 'Begin Copula: Clayton, Margina: bimodal_gaussian, CM Flow only'
 
-# python CM_Flow.py \
-# --exp_name CM_Flow_Clayton_bimodal_gaussian_cm_flow_only \
-# --epochs 50 \
-# --batch-size 100 \
-# --copula clayton \
-# --marginal_1 bimodal_gaussian \
-# --marginal_2 bimodal_gaussian \
-# --theta 2 \
-# --obs 10000 \
-# --train_cm_flow
+python CM_Flow.py \
+--exp_name CM_Flow_Clayton_bimodal_gaussian_cm_flow_only \
+--epochs 50 \
+--batch-size 100 \
+--copula clayton \
+--marginal_1 bimodal_gaussian \
+--marginal_2 bimodal_gaussian \
+--theta 2 \
+--obs 10000 \
+--train_cm_flow
 
 # echo 'Begin Copula: Clayton, Margina: bimodal_gaussian, Pretrain and CM Flow'
 
@@ -79,42 +79,42 @@ set -e
 # --train_cm_flow \
 # --pretrain_models
 
-echo 'Begin Copula: gumbel Marginal: bimodal_gaussian Pretrain only'
+# echo 'Begin Copula: gumbel Marginal: bimodal_gaussian Pretrain only'
 
-python CM_Flow.py \
---exp_name CM_Flow_gumbel_bimodal_gaussian_pretr_only \
---epochs 50 \
---batch-size 100 \
---copula gumbel \
---marginal_1 bimodal_gaussian \
---marginal_2 bimodal_gaussian \
---theta 5 \
---obs 10000 \
---pretrain_models
+# python CM_Flow.py \
+# --exp_name CM_Flow_gumbel_bimodal_gaussian_pretr_only \
+# --epochs 50 \
+# --batch-size 100 \
+# --copula gumbel \
+# --marginal_1 bimodal_gaussian \
+# --marginal_2 bimodal_gaussian \
+# --theta 5 \
+# --obs 10000 \
+# --pretrain_models
 
-echo 'Begin Copula: gumbel, Margina: bimodal_gaussian, CM Flow only'
+# echo 'Begin Copula: gumbel, Margina: bimodal_gaussian, CM Flow only'
 
-python CM_Flow.py \
---exp_name CM_Flow_gumbel_bimodal_gaussian_cm_flow_only \
---epochs 50 \
---batch-size 100 \
---copula gumbel \
---marginal_1 bimodal_gaussian \
---marginal_2 bimodal_gaussian \
---theta 5 \
---obs 10000 \
---train_cm_flow
+# python CM_Flow.py \
+# --exp_name CM_Flow_gumbel_bimodal_gaussian_cm_flow_only \
+# --epochs 50 \
+# --batch-size 100 \
+# --copula gumbel \
+# --marginal_1 bimodal_gaussian \
+# --marginal_2 bimodal_gaussian \
+# --theta 5 \
+# --obs 10000 \
+# --train_cm_flow
 
-echo 'Begin Copula: gumbel, Margina: bimodal_gaussian, Pretrain and CM Flow'
+# echo 'Begin Copula: gumbel, Margina: bimodal_gaussian, Pretrain and CM Flow'
 
-python CM_Flow.py \
---exp_name CM_Flow_gumbel_bimodal_gaussian_pretr_cm \
---epochs 50 \
---batch-size 100 \
---copula gumbel \
---marginal_1 bimodal_gaussian \
---marginal_2 bimodal_gaussian \
---theta 5 \
---obs 10000 \
---train_cm_flow \
---pretrain_models
+# python CM_Flow.py \
+# --exp_name CM_Flow_gumbel_bimodal_gaussian_pretr_cm \
+# --epochs 50 \
+# --batch-size 100 \
+# --copula gumbel \
+# --marginal_1 bimodal_gaussian \
+# --marginal_2 bimodal_gaussian \
+# --theta 5 \
+# --obs 10000 \
+# --train_cm_flow \
+# --pretrain_models
