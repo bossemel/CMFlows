3e3e5ddbfee2fb4edf9d10d17848e37ddfe9d5dd
bossemel <18574762+bossemel@users.noreply.github.com>
2020-11-02 15:39:24 +0100

    removed 1step functionality

--- CM_Flow.py
+++ CM_Flow.py
@@ -115,137 +115,135 @@ def train_and_plot(args, dataset, data_loaders, disable_tqdm=False, error_bars=F
     model.to(args.device)
 
     # Pretrain models individually, with cop_flow using the outputs of marg_flow as inputs
-    if args.pretrain_models:
-        # Train marg_flows
-        args.optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        args.scheduler = optim.lr_scheduler.CosineAnnealingLR(args.optimizer, args.epochs) #, args.num_training_steps, 0)
+    # Train marg_flows
+    args.optimizer = optim.Adam(model.parameters(), lr=args.lr_m, weight_decay=args.weight_decay_m)
+    args.scheduler = optim.lr_scheduler.CosineAnnealingLR(args.optimizer, args.epochs) #, args.num_training_steps, 0)
 
-        for param in model.marg_flow_2.parameters():
-            param.requires_grad = False
-        for param in model.cop_flow.parameters():
-            param.requires_grad = False
-        best_dict_marg_flow_1, test_dict = train_val(model=model,
-                                                     model_name='marg_flow_1',
-                                                     args=args,
-                                                     data_loaders=data_loaders,
-                                                     dataset=dataset,
-                                                     transform_inputs=True,
-                                                     disable_tqdm=disable_tqdm,
-                                                     error_bars=error_bars,
-                                                     rvine=rvine,
-                                                     cm_flow=True)
+    for param in model.marg_flow_2.parameters():
+        param.requires_grad = False
+    for param in model.cop_flow.parameters():
+        param.requires_grad = False
+    best_dict_marg_flow_1, test_dict = train_val(model=model,
+                                                 model_name='marg_flow_1',
+                                                 args=args,
+                                                 data_loaders=data_loaders,
+                                                 dataset=dataset,
+                                                 transform_inputs=True,
+                                                 disable_tqdm=disable_tqdm,
+                                                 error_bars=error_bars,
+                                                 rvine=rvine,
+                                                 cm_flow=True)
 
-        model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
-                           best_dict_marg_flow_1['best_validation_epoch'])
-        visualize1D(model=model.marg_flow_1,
-                    epoch=best_dict_marg_flow_1['best_validation_epoch'],
-                    args=args,
-                    best_val=True,
-                    name='marg_flow_1')
-        for param in model.marg_flow_1.parameters():
-            param.requires_grad = False
-        for param in model.marg_flow_2.parameters():
-            param.requires_grad = True
-        #args.optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=args.betas, weight_decay=args.weight_decay)
-        args.optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        args.scheduler = optim.lr_scheduler.CosineAnnealingLR(args.optimizer, args.epochs) #, args.num_training_steps, 0)
+    model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
+                       best_dict_marg_flow_1['best_validation_epoch'])
+    visualize1D(model=model.marg_flow_1,
+                epoch=best_dict_marg_flow_1['best_validation_epoch'],
+                args=args,
+                best_val=True,
+                name='marg_flow_1')
+    for param in model.marg_flow_1.parameters():
+        param.requires_grad = False
+    for param in model.marg_flow_2.parameters():
+        param.requires_grad = True
+    #args.optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=args.betas, weight_decay=args.weight_decay)
+    args.optimizer = optim.Adam(model.parameters(), lr=args.lr_m, weight_decay=args.weight_decay_m)
+    args.scheduler = optim.lr_scheduler.CosineAnnealingLR(args.optimizer, args.epochs) #, args.num_training_steps, 0)
 
+    best_dict_marg_flow_2, test_dict = train_val(model=model,
+                                                 model_name='marg_flow_2',
+                                                 args=args,
+                                                 data_loaders=data_loaders,
+                                                 dataset=dataset,
+                                                 transform_inputs=True,
+                                                 disable_tqdm=disable_tqdm,
+                                                 error_bars=error_bars,
+                                                 rvine=rvine,
+                                                 cm_flow=True)
 
-        best_dict_marg_flow_2, test_dict = train_val(model=model,
-                                                     model_name='marg_flow_2',
-                                                     args=args,
-                                                     data_loaders=data_loaders,
-                                                     dataset=dataset,
-                                                     transform_inputs=True,
-                                                     disable_tqdm=disable_tqdm,
-                                                     error_bars=error_bars,
-                                                     rvine=rvine,
-                                                     cm_flow=True)
+    model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
+                       best_dict_marg_flow_2['best_validation_epoch'])
+    visualize1D(model=model.marg_flow_2,
+                epoch=best_dict_marg_flow_2['best_validation_epoch'],
+                args=args,
+                best_val=True,
+                name='marg_flow_2')
+    # Visualize DDFS transformations
+    if not error_bars and not rvine:
+        visualize_marg_flow_output(model, dataset, args)
 
-        model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
-                           best_dict_marg_flow_2['best_validation_epoch'])
-        visualize1D(model=model.marg_flow_2,
-                    epoch=best_dict_marg_flow_2['best_validation_epoch'],
-                    args=args,
-                    best_val=True,
-                    name='marg_flow_2')
-        # Visualize DDFS transformations
-        if not error_bars and not rvine:
-            visualize_marg_flow_output(model, dataset, args)
+    for param in model.marg_flow_2.parameters():
+        param.requires_grad = False
+    for param in model.cop_flow.parameters():
+        param.requires_grad = True
+    # Train cop_flow
+    # args.optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=args.betas, weight_decay=args.weight_decay)
+    args.optimizer = optim.Adam(model.parameters(), lr=args.lr_c, weight_decay=args.weight_decay_c)
+    args.scheduler = optim.lr_scheduler.CosineAnnealingLR(args.optimizer, args.epochs) #, args.num_training_steps, 0)
+    model.cop_flow.train()
 
-        for param in model.marg_flow_2.parameters():
-            param.requires_grad = False
-        for param in model.cop_flow.parameters():
-            param.requires_grad = True
-        # Train cop_flow
-        # args.optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=args.betas, weight_decay=args.weight_decay)
-        args.optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        args.scheduler = optim.lr_scheduler.CosineAnnealingLR(args.optimizer, args.epochs) #, args.num_training_steps, 0)
-        model.cop_flow.train()
+    best_dict_cop_flow, test_dict = train_val(model,
+                                              model_name='cop_flow',
+                                              args=args,
+                                              data_loaders=data_loaders,
+                                              dataset=dataset,
+                                              test_dict=test_dict,
+                                              transform_inputs=True,
+                                              disable_tqdm=disable_tqdm,
+                                              error_bars=error_bars,
+                                              rvine=rvine,
+                                              cm_flow=True)
 
-        best_dict_cop_flow, test_dict = train_val(model,
-                                                  model_name='cop_flow',
-                                                  args=args,
-                                                  data_loaders=data_loaders,
-                                                  dataset=dataset,
-                                                  test_dict=test_dict,
-                                                  transform_inputs=True,
-                                                  disable_tqdm=disable_tqdm,
-                                                  error_bars=error_bars,
-                                                  rvine=rvine,
-                                                  cm_flow=True)
+    model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
+                       best_dict_cop_flow['best_validation_epoch'])
 
-        model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
-                           best_dict_cop_flow['best_validation_epoch'])
+    best_dict = best_dict_cop_flow
 
-        best_dict = best_dict_cop_flow
+    if not error_bars and not rvine:
+        visualize_cop_flow_output(model, dataset, args)
 
-        if not error_bars and not rvine:
-            visualize_cop_flow_output(model, dataset, args)
-
-        # Gather test losses and save statistics
-        test_losses = {key: [np.mean(value)] for key, value in
-                       test_dict.items()}  # save test set metrics in dict format
-        sep = '_'
-        epochs = sep.join(list([str(best_dict_marg_flow_1['best_validation_epoch']),
-                                str(best_dict_marg_flow_2['best_validation_epoch']),
-                                str(best_dict_cop_flow['best_validation_epoch'])]))
-        if not rvine:
-            save_statistics(experiment_log_dir=args.experiment_logs, filename='test_summary.csv',
-                            # save test set metrics on disk in .csv format
-                            stats_dict=test_losses, current_epoch=0, continue_from_mode=error_bars, test_epoch=epochs)
-
-    # Train the CM Flow
-    if args.train_cm_flow:
-
-        # Set optimizer
-        args.optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=args.betas)
-
-        # Train model, and perform validation and test
-        best_dict, test_dict = train_val(model,
-                                         model_name='CM_Flow',
-                                         args=args,
-                                         data_loaders=data_loaders,
-                                         dataset=dataset,
-                                         disable_tqdm=disable_tqdm,
-                                         error_bars=error_bars)
-
-        model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
-                           best_dict['best_validation_epoch'])
-
-        if not error_bars and not rvine:
-            visualize_CM_Flow_output(model, dataset, args)
-
-        # Visualize DDFS transformations
-        if not error_bars and not rvine:
-            visualize_marg_flow_output(model, dataset, args)
-
-        # Gather test losses and save statistics
-        test_losses = {key: [np.mean(value)] for key, value in
-                       test_dict.items()}  # save test set metrics in dict format
+    # Gather test losses and save statistics
+    test_losses = {key: [np.mean(value)] for key, value in
+                   test_dict.items()}  # save test set metrics in dict format
+    sep = '_'
+    epochs = sep.join(list([str(best_dict_marg_flow_1['best_validation_epoch']),
+                            str(best_dict_marg_flow_2['best_validation_epoch']),
+                            str(best_dict_cop_flow['best_validation_epoch'])]))
+    if not rvine:
         save_statistics(experiment_log_dir=args.experiment_logs, filename='test_summary.csv',
                         # save test set metrics on disk in .csv format
-                        stats_dict=test_losses, current_epoch=0, continue_from_mode=error_bars, test_epoch=best_dict['best_validation_epoch'])
+                        stats_dict=test_losses, current_epoch=0, continue_from_mode=error_bars, test_epoch=epochs)
+
+    # # Train the CM Flow
+    # if args.train_cm_flow:
+
+    #     # Set optimizer
+    #     args.optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=args.betas)
+
+    #     # Train model, and perform validation and test
+    #     best_dict, test_dict = train_val(model,
+    #                                      model_name='CM_Flow',
+    #                                      args=args,
+    #                                      data_loaders=data_loaders,
+    #                                      dataset=dataset,
+    #                                      disable_tqdm=disable_tqdm,
+    #                                      error_bars=error_bars)
+
+    #     model = load_model(model, args.experiment_saved_models, 'best_epoch_model',
+    #                        best_dict['best_validation_epoch'])
+
+    #     if not error_bars and not rvine:
+    #         visualize_CM_Flow_output(model, dataset, args)
+
+    #     # Visualize DDFS transformations
+    #     if not error_bars and not rvine:
+    #         visualize_marg_flow_output(model, dataset, args)
+
+    #     # Gather test losses and save statistics
+    #     test_losses = {key: [np.mean(value)] for key, value in
+    #                    test_dict.items()}  # save test set metrics in dict format
+    #     save_statistics(experiment_log_dir=args.experiment_logs, filename='test_summary.csv',
+    #                     # save test set metrics on disk in .csv format
+    #                     stats_dict=test_losses, current_epoch=0, continue_from_mode=error_bars, test_epoch=best_dict['best_validation_epoch'])
     return best_dict
 
 
--- CM_modules/options.py
+++ CM_modules/options.py
@@ -17,10 +17,10 @@ class TrainOptions():
         # CM Options
 
         # Architecture
-        parser.add_argument(
-            '--pretrain_models', action='store_true', help='first trains marginal flow, then copula flow')
-        parser.add_argument(
-            '--train_cm_flow', action='store_true', help='whether to train combined CM Flow')
+        # parser.add_argument(
+        #     '--pretrain_models', action='store_true', help='first trains marginal flow, then copula flow')
+        # parser.add_argument(
+        #     '--train_cm_flow', action='store_true', help='whether to train combined CM Flow')
         parser.add_argument(
             '--conditional_copula', action='store_true', help='estimates the conditional copula')
 
@@ -30,7 +30,9 @@ class TrainOptions():
         parser.add_argument(
             '--epochs', type=int, default=100, help='number of epochs to train (default: 100)')
         parser.add_argument(
-            '--lr', type=float, default=0.001, help='learning rate (default: 0.0001)')
+            '--lr_c', type=float, default=0.01, help='learning rate (default: 0.0001)')
+        parser.add_argument(
+            '--lr_m', type=float, default=0.0001, help='learning rate (default: 0.0001)')
         parser.add_argument(
             '--no-cuda', action='store_true', default=False, help='disables CUDA training')
         parser.add_argument(
@@ -40,11 +42,19 @@ class TrainOptions():
         parser.add_argument(
             '--clip', type=float, default=5.0)
         parser.add_argument(
-            '--weight_decay', type=int, default=1e-10, help='adam optimizer weight decay')
+            '--weight_decay_c', type=int, default=1e-10, help='adam optimizer weight decay')
+        parser.add_argument(
+            '--weight_decay_m', type=int, default=1e-10, help='adam optimizer weight decay')
         parser.add_argument(
             '--early_stopping', action='store_true', default=False, help='stops training after 10 unsuccessfull epochs')
         parser.add_argument(
             '--error_bars', action='store_true', default=False, help='trains 10 times and return the standard deviation and mean of test loss')
+        parser.add_argument(
+            '--random_search', action='store_true', help='random search over hyperparameters')
+        parser.add_argument(
+            '--continue_from', type=int, default=0, help='continue random search from iteration number')
+        parser.add_argument(
+            '--flow_type', type=str, default='cop_flow', choices=['cop_flow', 'marg_flow'])
 
         # Dataset options
         parser.add_argument(
@@ -104,17 +114,15 @@ class TrainOptions():
 
         # NSF Options marginal
         parser.add_argument(
-            '--n_layers_m', type=int, default=10, help='Number of spline layers in flow')
+            '--n_layers_m', type=int, default=15, help='Number of spline layers in flow')
         parser.add_argument(
             '--hidden_units_m', type=int, default=128, help='Number of hidden units in spline layer')
         parser.add_argument(
-            '--n_blocks_m', type=int, default=2, help='Number of residual blocks in each spline layer')
+            '--n_blocks_m', type=int, default=3, help='Number of residual blocks in each spline layer')
         parser.add_argument(
             '--n_bins_m', type=int, default=4, help='Number of bins in piecewise spline transform')
         parser.add_argument(
-            '--dropout_m', type=float, default=0.15, help='Dropout probability in flow')
-        parser.add_argument(
-            '--tail_bound_m', type=float, default=8, help='Bounds of spline region')
+            '--tail_bound_m', type=float, default=4, help='Bounds of spline region')
 
         # Save options
         parser.add_argument(
